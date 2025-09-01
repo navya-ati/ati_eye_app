67f9808f0b413d6be0a9fe7f7d01197e42915114
navya-ati <navya.srivastava@atimotors.com>
2025-09-01 11:32:37 +0530

    Update app_ati_eye.py

--- app_ati_eye.py
+++ app_ati_eye.py
@@ -1,5 +1,3 @@
-print("Importing libraries...")
-
 import time
 import cv2
 from core_app import AtiEyeApp, APP_VERSION
@@ -7,36 +5,25 @@ from core_app import AtiEyeApp, APP_VERSION
 print(f"Initializing global AtiEye application v{APP_VERSION}!")
 
 def main():
-    print("Starting the main AtiEye application...")
-
-    # Create AtiEye app instance
     eye = AtiEyeApp()
+    start_time = time.time()
 
-    start = time.time()
     while True:
         if eye.cam_capture.is_new_data:
-            start = time.time()
-            image_src, timestamp, frame_id = eye.cam_capture.get_data()
-            if timestamp is None:
-                eye.logger.info(f"Invalid image in frame: {frame_id}! Exiting the app!")
-                break
+            frame_data = eye.cam_capture.get_data()
+            if frame_data is None:
+                continue
 
-            # Run inference on full frame
+            image_src, timestamp, frame_id = frame_data
+
+            # Run inference
             yolo_result = eye.run_inference(image_src)
 
-            # Only green rectangles are drawn in core_app.py
-            # Update inference history
-            eye.update_inference_history(yolo_result.detected_object)
-            eye.process_inference_history()
-            eye.log_image(yolo_result, frame_id)
-
-            # Log processing info
-            proc_time = round(time.time() - start, 3)
-            eye.logger.info(f"img: {frame_id}, process_time - {proc_time} secs")
-
+            proc_time = round(time.time() - start_time, 3)
+            eye.logger.info(f"Frame {frame_id} processed in {proc_time} sec")
+            start_time = time.time()
         else:
-            diff_time = round(time.time() - start, 3)
-            eye.logger.info(f"No new data for {diff_time} seconds")
+            time.sleep(0.05)
 
 if __name__ == "__main__":
     try:
@@ -44,4 +31,3 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print("\n[INFO] Stopping Ati-Eye gracefully...")
         cv2.destroyAllWindows()
-
